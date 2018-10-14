a8d2a6435cf03f22a23ce046f4c62ccb2b26818d
Mouse.Yu <@Mouse.Yu>
2018-10-14 13:22:29 +0800

    first commit to github

---
+++ fast-project/fast-web/src/main/java/com/mouse/FastProjectApplication.java
@@ -0,0 +1,12 @@
+package com.mouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastProjectApplication.class, args);
+    }
+}
